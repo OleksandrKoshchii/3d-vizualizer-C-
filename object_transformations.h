4281a3533490056ba4d69de5b9fd8233bb66a050
Yaroslav Danko <dankoyar@cvut.cz>
2025-05-04 13:26:32 +0300

    Refactor: Reorganized project into folders

--- object_transformations.h
+++
@@ -1,9 +0,0 @@
-#ifndef __OBJECT_TRANSFORMATIONS__
-#define __OBJECT_TRANSFORMATIONS__
-
-#include "read_stl.h"
-
-void rotate_obj_horizontal(obj_t* obj, float angle);
-void rotate_obj_vertical(obj_t* obj,float angle);
-
-#endif
